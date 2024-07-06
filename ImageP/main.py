f305a1ea1be12e65ec3aa68f77678180e4735ca7
linuslau <liukezhao@gmail.com>
2024-07-06 20:11:28 +0800

    Rename MainWindows to ImageP

--- ImageP/main.py
+++ ImageP/main.py
@@ -4,14 +4,12 @@ from utils.menu_populate import populate_menu
 import sys
 import os
 
-
 def load_menu_order(menu_path):
     order_file = os.path.join(menu_path, 'order.txt')
     if not os.path.exists(order_file):
         return []
     with open(order_file, 'r') as f:
         return [line.strip() for line in f if line.strip()]
-
 
 def add_menu_item(menu, path, is_folder):
     if is_folder:
@@ -21,15 +19,21 @@ def add_menu_item(menu, path, is_folder):
         action = menu.addAction(os.path.basename(path).replace('.py', ''))
         action.triggered.connect(lambda: handle_menu_click(path))
 
-
 def handle_menu_click(file_path):
     # 根据文件路径导入相应模块并调用预定义的函数
     module_name = os.path.splitext(os.path.basename(file_path))[0]
-    module_spec = __import__('menu.' + module_name, fromlist=[module_name])
-    if hasattr(module_spec, 'menu_click'):
-        window = module_spec.menu_click()
-        if window:
-            window.show()
+    try:
+        module_spec = __import__(f'menu.{module_name}', fromlist=[module_name])
+        if hasattr(module_spec, 'menu_click'):
+            window = module_spec.menu_click()
+            if window:
+                window.setWindowTitle("ImageP")  # 设置窗口标题为 "ImageP"
+                window.show()
+    except ModuleNotFoundError as e:
+        print(f"Module not found: {e}")
+    except Exception as e:
+        print(f"Error while handling menu click: {e}")
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
@@ -56,9 +60,9 @@ def main():
             if (os.path.isdir(item_path) and item != '__pycache__') or (item.endswith('.py') and item != '__init__.py'):
                 add_menu_item(ui.menubar, item_path, os.path.isdir(item_path))
 
+    MainWindow.setWindowTitle("ImageP")  # 设置主窗口标题为 "ImageP"
     MainWindow.show()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
