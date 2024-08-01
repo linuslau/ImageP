c999f1126e358a72266cb1871b76f0a838244c3a
linuslau <liukezhao@gmail.com>
2024-08-01 15:16:20 +0800

    Rectangle and ellipe drag does not work

--- TestOpenCV/testPYQTG.py
+++ TestOpenCV/testPYQTG.py
@@ -92,8 +92,8 @@ class CustomViewBox(pg.ViewBox):
             return
 
         # Clear existing lines before starting a new one
-        if self.shape_type != "polygon" and self.shape_type != "dynamic_polygon":
-            self.clear_lines()
+        # if self.shape_type != "polygon" and self.shape_type != "dynamic_polygon":
+            # self.clear_lines()
 
         if self.shape_type == "polygon":
             if event.button() == QtCore.Qt.LeftButton:
