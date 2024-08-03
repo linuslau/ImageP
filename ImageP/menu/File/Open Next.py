f0e40edab71d828e4b9b09773cb16075b5218010
linuslau <liukezhao@gmail.com>
2024-08-03 10:48:42 +0800

    Move test Raw file open to real place

--- ImageP/menu/File/Open Next.py
+++
@@ -1,8 +0,0 @@
-from TestOpenCV.testPYQTG import create_and_show_image_with_rect
-
-def handle_click():
-    create_and_show_image_with_rect()
-
-# def main():
-#    create_and_show_image_with_rect()
-# main()
