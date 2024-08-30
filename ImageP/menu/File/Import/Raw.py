711bc4beb2b65f9ebbc0d83e1e082078de8c0b42
linuslau <liukezhao@gmail.com>
2024-08-30 20:21:35 +0800

    Image property dialog default value

--- ImageP/menu/File/Import/Raw.py
+++ ImageP/menu/File/Import/Raw.py
@@ -1,9 +1,23 @@
 import sys
+import json
+import os
 from PyQt5.QtWidgets import (
     QApplication, QFileDialog, QDialog, QVBoxLayout, QLabel, QComboBox, QLineEdit, QCheckBox, QPushButton, QHBoxLayout
 )
 from PyQt5.QtCore import QTimer
 from TestOpenCV.testPYQTG import create_and_show_image_with_rect
+
+CONFIG_FILE = "import_dialog_config.json"
+
+def load_config():
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, 'r') as f:
+            return json.load(f)
+    return {}
+
+def save_config(config):
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump(config, f, indent=4)
 
 def show_import_dialog():
     dialog = QDialog()
@@ -11,6 +25,9 @@ def show_import_dialog():
 
     # 设置对话框的最小宽度
     dialog.setMinimumWidth(600)
+
+    # 加载上次保存的配置
+    config = load_config()
 
     layout = QVBoxLayout()
 
@@ -22,6 +39,7 @@ def show_import_dialog():
         "32-bit Real", "64-bit Real", "24-bit RGB", "24-bit RGB Planar", "24-bit BGR",
         "24-bit Integer", "32-bit ARGB", "32-bit ABGR", "1-bit Bitmap"
     ])
+    image_type_combo.setCurrentText(config.get('image_type', '8-bit'))
     layout.addWidget(image_type_label)
     layout.addWidget(image_type_combo)
 
@@ -29,9 +47,11 @@ def show_import_dialog():
     width_label = QLabel("Width (pixels):")
     width_input = QLineEdit()
     width_input.setPlaceholderText("Enter width, can't be 0")
+    width_input.setText(str(config.get('width', '')))
     height_label = QLabel("Height (pixels):")
     height_input = QLineEdit()
     height_input.setPlaceholderText("Enter height, can't be 0")
+    height_input.setText(str(config.get('height', '')))
     layout.addWidget(width_label)
     layout.addWidget(width_input)
     layout.addWidget(height_label)
@@ -41,6 +61,7 @@ def show_import_dialog():
     offset_label = QLabel("Offset to first image (bytes):")
     offset_input = QLineEdit()
     offset_input.setPlaceholderText("Enter offset, default 0")
+    offset_input.setText(str(config.get('offset', '')))
     layout.addWidget(offset_label)
     layout.addWidget(offset_input)
 
@@ -48,6 +69,7 @@ def show_import_dialog():
     num_images_label = QLabel("Number of images:")
     num_images_input = QLineEdit()
     num_images_input.setPlaceholderText("Enter number, default 1")
+    num_images_input.setText(str(config.get('num_images', '')))
     layout.addWidget(num_images_label)
     layout.addWidget(num_images_input)
 
@@ -55,14 +77,19 @@ def show_import_dialog():
     gap_label = QLabel("Gap between images (bytes):")
     gap_input = QLineEdit()
     gap_input.setPlaceholderText("Enter gap, default 0")
+    gap_input.setText(str(config.get('gap', '')))
     layout.addWidget(gap_label)
     layout.addWidget(gap_input)
 
     # Checkboxes
     white_zero_checkbox = QCheckBox("White is zero")
+    white_zero_checkbox.setChecked(config.get('white_zero', False))
     little_endian_checkbox = QCheckBox("Little-endian byte order")
+    little_endian_checkbox.setChecked(config.get('little_endian', False))
     open_all_files_checkbox = QCheckBox("Open all files in folder")
+    open_all_files_checkbox.setChecked(config.get('open_all_files', False))
     virtual_stack_checkbox = QCheckBox("Use virtual stack")
+    virtual_stack_checkbox.setChecked(config.get('virtual_stack', False))
     layout.addWidget(white_zero_checkbox)
     layout.addWidget(little_endian_checkbox)
     layout.addWidget(open_all_files_checkbox)
@@ -99,6 +126,10 @@ def show_import_dialog():
             'open_all_files': open_all_files_checkbox.isChecked(),
             'virtual_stack': virtual_stack_checkbox.isChecked()
         }
+
+        # Save the configuration to file
+        save_config(params)
+
         return params
     else:
         return None
